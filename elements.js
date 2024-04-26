baa15e46dce71e9f36b74866f3725177b26949fd
Larissa Gramacho <139820314+gramacholarissa@users.noreply.github.com>
2024-04-26 14:59:36 -0300

    Create elements.js

---
+++ elements.js
@@ -0,0 +1,8 @@
+export const elements = {
+
+    name: '[data-test="input-name"]',
+    password: '[data-test="email"]',
+    email: '[data-test="password"]',
+    btn_link_register: '[data-test="register-button"]',
+    btn_register: '[data-test="submit-button"]'
+}
