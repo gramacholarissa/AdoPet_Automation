c7ddd1e9812d5bb0c2a96b659a7d4fd6d4c6d66f
Larissa Gramacho <139820314+gramacholarissa@users.noreply.github.com>
2024-04-26 14:58:42 -0300

    Create user_registraion.cy.js

---
+++ user_registraion.cy.js
@@ -0,0 +1,44 @@
+describe('user registration', () => {
+
+
+  beforeEach(() => {
+    cy.visit('https://adopet-frontend-cypress.vercel.app/');
+  })
+
+  it('sucessful user registration', () => {
+
+
+    cy.new_user(faker.person.fullName(), faker.internet.email(), faker.internet.password());
+
+
+  })
+
+  it('Display feedback when all required fields are not completed', () => {
+
+
+    cy.display_feedback_all();
+
+
+  })
+
+
+  it('Display feedbak when passowrds do not match', () => {
+
+
+    cy.display_feedback_passwords_do_not_match();
+
+
+  })
+
+  it('display feedback when password does not meet requirements', () => {
+
+
+    cy.display_feedback_password();
+
+
+  })
+
+
+}
+
+)
